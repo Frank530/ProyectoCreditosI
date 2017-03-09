d1ce6b8de203c638d59f5149ac75a15b0c2c9646
Cristian Piamba <>
2017-03-09 17:17:51 -0500

    Version 1.0

--- src/main/java/co/edu/ims/AppCompraventa/AppMain.java
+++ src/main/java/co/edu/ims/AppCompraventa/AppMain.java
@@ -1,6 +1,7 @@
 package co.edu.ims.AppCompraventa;
 
 import co.edu.ims.compraventa.Categoria;
+import co.edu.ims.compraventa.Cliente;
 import co.edu.ims.compraventa.Producto;
 import com.google.gson.Gson;
 import java.util.ArrayList;
@@ -17,13 +18,19 @@ public class AppMain {
         List<Categoria> categorias=new ArrayList<>();
         categorias.add(aseo);
         
+        Cliente nombre = new Cliente(1, "Cristian", "Cra 12 # 17-93", 123456789, "Popayan", "3123123123");
+        List<Cliente> cliente = new ArrayList<>();
+        cliente.add(nombre);
+        
         Producto escoba = new Producto(1, "Escoba", 20000, categorias);
+        
         Gson gson = new Gson();
-        String json = gson.toJson(escoba);
-        System.out.println("Datos producto"+json);
-        
-        
-        
+        String jscategotia = gson.toJson(aseo);
+        System.out.println("Datos del Producto:"+jscategotia);
+        String jsonproducto = gson.toJson(escoba);
+        System.out.println("Datos producto"+jsonproducto);
+        String jscliente = gson.toJson(cliente);
+        System.out.println("Datos del Cliente:"+jscliente);
         
         
     }
