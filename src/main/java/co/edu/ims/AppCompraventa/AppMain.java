25774e0a54d81ae5674ab4b2ce3d2259431e04f5
Frank <>
2017-03-09 16:47:38 -0500

    Version 1.0

--- src/main/java/co/edu/ims/AppCompraventa/AppMain.java
+++ src/main/java/co/edu/ims/AppCompraventa/AppMain.java
@@ -2,6 +2,7 @@ package co.edu.ims.AppCompraventa;
 
 import co.edu.ims.compraventa.Categoria;
 import co.edu.ims.compraventa.Producto;
+import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,13 @@ public class AppMain {
         categorias.add(aseo);
         
         Producto escoba = new Producto(1, "Escoba", 20000, categorias);
-        System.out.println(escoba.getId());
+        Gson gson = new Gson();
+        String json = gson.toJson(escoba);
+        System.out.println("Datos producto"+json);
+        
+        
+        
+        
         
     }
     
